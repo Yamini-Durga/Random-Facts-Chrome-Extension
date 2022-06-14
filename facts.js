16fe14dc7697dd4b4c5636c6b1443a0e813c5b9b
Yamini-Durga <101850697+Yamini-Durga@users.noreply.github.com>
2022-06-14 19:57:06 +0530

    Update facts.js

--- facts.js
+++ facts.js
@@ -2,7 +2,7 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'b4833cf298msh443983ff9b2e848p19ac89jsn26e9cf0f4dc8',
+		'X-RapidAPI-Key': 'key',
 		'X-RapidAPI-Host': 'random-facts2.p.rapidapi.com'
 	}
 };
